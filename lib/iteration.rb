ced869087d318d0cec2f0f913a38a6fbff96d5d0
Rianna Cleary <rianna.cleary@gmail.com>
2019-11-08 19:56:59 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -11,6 +11,12 @@ def join_ingredients(src)
   row_index = 0 
   while row_index < src.length do 
     element_index = 0 
+    while element_index < src[row_index].length do 
+      new_array << `I love #{element_index[0]} and #{element_index[1]} on my pizza`
+      element_index += 1
+    end 
+    row_index += 1 
+  end 
 end
 
 def find_greater_pair(src)
